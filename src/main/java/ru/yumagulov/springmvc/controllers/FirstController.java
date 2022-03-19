f9e60f9e3d2c47cf59d9f0964b3c1e48ed980bef
Il'dar <yumagulov.ildar@gmail.com>
2022-03-19 20:12:11 +0500

    interaction with the model
    Passing Data from Controller to View

--- src/main/java/ru/yumagulov/springmvc/controllers/FirstController.java
+++ src/main/java/ru/yumagulov/springmvc/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package ru.yumagulov.springmvc.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,5 +26,30 @@ public class FirstController {
                               @RequestParam(value = "surname",required = false)String surname){
         System.out.println("Goodbye " + name + " " + surname);
         return "first/goodbye";
+    }
+    @GetMapping("/calculator")
+    public String goodByePage(@RequestParam(value = "a") int a,
+                              @RequestParam(value = "b")int b,
+                              @RequestParam(value = "action")String action,
+                              Model model){
+        double result;
+        switch (action) {
+            case "multiplication":
+                result = a * b;
+                break;
+            case "division":
+                result = a / (double)b;
+                break;
+            case "subtraction":
+                result = a - b;
+                break;
+            case "addition":
+                result = a + b;
+                break;
+            default:
+                result = Double.NaN;
+        }
+        model.addAttribute("result",result);
+        return "first/calculator";
     }
 }
